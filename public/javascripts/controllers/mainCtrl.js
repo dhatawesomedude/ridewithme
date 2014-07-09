3390baa83e56a0898c01837c27b18d7016f53650
deb2fast <deb2fast@gmail.com>
2014-07-10 02:01:48 +0800

    main controller using tripservice

--- public/javascripts/controllers/mainCtrl.js
+++ public/javascripts/controllers/mainCtrl.js
@@ -2,9 +2,21 @@
  * Created by Orlando on 30/6/2014.
  */
 angular.module('MainCtrl', [])
-    .controller('MainController',['$scope', function($scope)
+    .controller('MainController',['$scope', 'Trip', function($scope, Trip)
     {
     $scope.depature = "cyberjaya";
     $scope.destination = "Putrajaya";
+
+    $scope.searchResults = Trip.findTrips({destination : $scope.destination, depature : $scope.depature});
+    $scope.searchResults.$promise.then(function(result)
+    {
+        $scope.searchResults = result;
+
+    }
+    ,function(error)
+        {
+            console.log('error is' + error);
+        });
+
     }
     ]);
